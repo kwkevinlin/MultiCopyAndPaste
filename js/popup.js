a2e41265e1b22c4298d78b075fb6dada8cf792b6
Kevin Lin <kevinl@qualtrics.com>
2017-04-29 19:59:30 -0700

    Changed event listener from on click to on change

--- js/popup.js
+++ js/popup.js
@@ -6,10 +6,14 @@ $(document).ready(function() {
 	$("#clipboard_2").val(localStorage.getItem("clipboard_2"));
 	$("#clipboard_3").val(localStorage.getItem("clipboard_3"));
 
-	// Save clipboard values on click
-	$("body").on("click", function() {
+	// Save clipboard values on change
+	$("#clipboard_1").change(function () {
 		localStorage.setItem("clipboard_1", $("#clipboard_1").val());
+	});
+	$("#clipboard_2").change(function () {
 		localStorage.setItem("clipboard_2", $("#clipboard_2").val());
+	});
+	$("#clipboard_3").change(function () {
 		localStorage.setItem("clipboard_3", $("#clipboard_3").val());
 	});
 });
