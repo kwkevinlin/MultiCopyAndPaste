d2b7877dfc6f259e5fb0f87d5ec9b3408e0fa20f
Kevin Lin <kevinl@qualtrics.com>
2017-04-29 22:01:22 -0700

    Added basic pasting code

--- js/content_script.js
+++ js/content_script.js
@@ -2,8 +2,27 @@
 $(document).ready(function() {
     console.log("Content Script loaded");
 
-    // TODO: Add paste function with key bind
-    $("body").click(function() {
-        console.log("Clicked ID : ", document.activeElement.id);  
+    // TODO: Strenghten logic on cmd + v + 1/2/3 pasting
+    // Ie. Catch paste, catch cmd + 1, or incorporate catching in key capture conditionals below
+    $(document).bind('paste', function(evt) {
+        console.log('Overriding pasting function!');
+        evt.preventDefault();
     });
+
+    var map = {}; // You could also use an array
+    onkeydown = onkeyup = function(evt){
+        map[evt.keyCode] = evt.type == 'keydown';
+    
+        // Key capture conditionals
+        // cmd = 91
+        // v = 86
+        // ctrl = 17
+        if (map[91] && map[86]){  // CMD + V
+            console.log('Command V');
+            if (document.activeElement.id == "first_name") {  // Testing safeguard
+                document.activeElement.value = "test";
+            }
+        }
+    };
+
 });
