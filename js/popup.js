cb160753ad31109d1fbc6ab11e635a51e7a73219
Kevin Lin <kevinl@qualtrics.com>
2017-04-30 16:52:35 -0700

    Changed localStorage to chrome.storage.sync

--- js/popup.js
+++ js/popup.js
@@ -1,20 +1,28 @@
 // popup.js
 
 $(document).ready(function() {
-	// Load stored clipboard values from localStorage if available
-	$("#clipboard_1").val(localStorage.getItem("clipboard_1"));
-	$("#clipboard_2").val(localStorage.getItem("clipboard_2"));
-	$("#clipboard_3").val(localStorage.getItem("clipboard_3"));
+	// Load stored clipboard values from storage if available
+	chrome.storage.sync.get(["clipboard_1", "clipboard_2", "clipboard_3"], function(obj) {
+    	$("#clipboard_1").val(obj.clipboard_1);
+    	$("#clipboard_2").val(obj.clipboard_2);
+    	$("#clipboard_3").val(obj.clipboard_3);
+    });
 
 	// Save clipboard values on change
 	$("#clipboard_1").change(function () {
-		localStorage.setItem("clipboard_1", $("#clipboard_1").val());
+		chrome.storage.sync.set({'clipboard_1': $("#clipboard_1").val()}, function() {
+			console.log('Settings 1 saved');
+		});
 	});
 	$("#clipboard_2").change(function () {
-		localStorage.setItem("clipboard_2", $("#clipboard_2").val());
+		chrome.storage.sync.set({'clipboard_2': $("#clipboard_2").val()}, function() {
+			console.log('Settings 2 saved');
+		});
 	});
 	$("#clipboard_3").change(function () {
-		localStorage.setItem("clipboard_3", $("#clipboard_3").val());
+		chrome.storage.sync.set({'clipboard_3': $("#clipboard_3").val()}, function() {
+			console.log('Settings 3 saved');
+		});
 	});
 });
 
