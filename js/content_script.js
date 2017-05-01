66878574e249a8aebb9e53b5fa6922a5a7c2b37c
lchao1 <liangchao.cl@gmail.com>
2017-04-30 20:37:42 -0400

    Added smart pasting functionality

--- js/content_script.js
+++ js/content_script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     var key = {}; // You could also use an array
     onkeydown = onkeyup = function(evt){
         key[evt.keyCode] = evt.type == 'keydown';
-    
+   
         // TODO: Replace current hotkeys with ctrl/cmd + v + 1/2/3
         // Will require evt.preventDefault() for "paste" and ctrl/cmd + 1/2/3 (tabs switching)
         //
@@ -16,27 +16,32 @@ $(document).ready(function() {
         // CMD = 91
         // CTRL = 17
         // U = 85
+        // I = 73
         // J = 74
-        // I = 75
         // V = 86
         if (key[17] && key[85]) {  // Ctrl + U
             chrome.storage.sync.get("clipboard_1", function(obj) {
                 console.log("Ctrl U: " + obj.clipboard_1);
-                document.activeElement.value = obj.clipboard_1;
+                cursorPos = $("#" + document.activeElement.id).prop("selectionStart");
+                original = document.activeElement.value;
+                document.activeElement.value = original.slice(0, cursorPos) + obj.clipboard_1 + original.slice(cursorPos);
+            });
+        }
+        else if (key[17] && key[73]) {  // Ctrl + I
+            chrome.storage.sync.get("clipboard_2", function(obj) {
+                console.log("Ctrl I: " + obj.clipboard_2);
+                cursorPos = $("#" + document.activeElement.id).prop("selectionStart");
+                original = document.activeElement.value;
+                document.activeElement.value = original.slice(0, cursorPos) + obj.clipboard_2 + original.slice(cursorPos);
             });
         }
         else if (key[17] && key[74]) {  // Ctrl + J
-            chrome.storage.sync.get("clipboard_2", function(obj) {
-                console.log("Ctrl J: " + obj.clipboard_2);
-                document.activeElement.value = obj.clipboard_2;
-            });
-        }
-        else if (key[17] && key[75]) {  // Ctrl + I
             chrome.storage.sync.get("clipboard_3", function(obj) {
-                console.log("Ctrl I: " + obj.clipboard_3);
-                document.activeElement.value = obj.clipboard_3;
+                console.log("Ctrl J: " + obj.clipboard_3);
+                cursorPos = $("#" + document.activeElement.id).prop("selectionStart");
+                original = document.activeElement.value;
+                document.activeElement.value = original.slice(0, cursorPos) + obj.clipboard_3 + original.slice(cursorPos);
             });
         }
     };
-
 });
