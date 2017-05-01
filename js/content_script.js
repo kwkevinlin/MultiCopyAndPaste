af5b3dc7283d7d323f80bd660aa727f40013f7a5
Kevin Lin <kevinl@qualtrics.com>
2017-04-30 18:00:55 -0700

    Added replace with selectionStart and End

--- js/content_script.js
+++ js/content_script.js
@@ -22,25 +22,28 @@ $(document).ready(function() {
         if (key[17] && key[85]) {  // Ctrl + U
             chrome.storage.sync.get("clipboard_1", function(obj) {
                 console.log("Ctrl U: " + obj.clipboard_1);
-                cursorPos = $("#" + document.activeElement.id).prop("selectionStart");
+                cursorStart = $("#" + document.activeElement.id).prop("selectionStart");
+                cursorEnd = $("#" + document.activeElement.id).prop("selectionEnd");
                 original = document.activeElement.value;
-                document.activeElement.value = original.slice(0, cursorPos) + obj.clipboard_1 + original.slice(cursorPos);
+                document.activeElement.value = original.slice(0, cursorStart) + obj.clipboard_1 + original.slice(cursorEnd);
             });
         }
         else if (key[17] && key[73]) {  // Ctrl + I
             chrome.storage.sync.get("clipboard_2", function(obj) {
                 console.log("Ctrl I: " + obj.clipboard_2);
-                cursorPos = $("#" + document.activeElement.id).prop("selectionStart");
+                cursorStart = $("#" + document.activeElement.id).prop("selectionStart");
+                cursorEnd = $("#" + document.activeElement.id).prop("selectionEnd");
                 original = document.activeElement.value;
-                document.activeElement.value = original.slice(0, cursorPos) + obj.clipboard_2 + original.slice(cursorPos);
+                document.activeElement.value = original.slice(0, cursorStart) + obj.clipboard_2 + original.slice(cursorEnd);
             });
         }
         else if (key[17] && key[74]) {  // Ctrl + J
             chrome.storage.sync.get("clipboard_3", function(obj) {
                 console.log("Ctrl J: " + obj.clipboard_3);
-                cursorPos = $("#" + document.activeElement.id).prop("selectionStart");
+                cursorStart = $("#" + document.activeElement.id).prop("selectionStart");
+                cursorEnd = $("#" + document.activeElement.id).prop("selectionEnd");
                 original = document.activeElement.value;
-                document.activeElement.value = original.slice(0, cursorPos) + obj.clipboard_3 + original.slice(cursorPos);
+                document.activeElement.value = original.slice(0, cursorStart) + obj.clipboard_3 + original.slice(cursorEnd);
             });
         }
     };
